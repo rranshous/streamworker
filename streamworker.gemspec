67fd6b0d684567d98ec9c66569da0692381c0731
Robby Ranshous <rranshous@gmail.com>
2015-02-20 18:47:58 -0500

    redis connection, deps, new version

--- streamworker.gemspec
+++ streamworker.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name          = 'streamworker'
-  s.version       = '0.0.1'
+  s.version       = '0.0.2'
   s.licenses      = ['BeerWare']
   s.summary       = "Work on events in a stream"
   s.description   = "Subscribe to eventstore stream, work on all events"
@@ -9,4 +9,6 @@ Gem::Specification.new do |s|
   s.files         = ["streamworker.rb"]
   s.homepage      = "https://github.com/rranshous/streamworker"
   s.require_paths = ['.']
+  s.add_dependency 'eventstore', '~> 0.1.2'
+  s.add_dependency 'redis-namespace', '~> 1.5.1'
 end
--- streamworker.rb
+++ streamworker.rb
@@ -1,26 +1,50 @@
 require 'eventstore'
+require 'redis-namespace'
+
 module StreamWorker
   def run!
-    default = 'http://0.0.0.0:2113'
-    @eventstore = EventStore::Client.new ENV['EVENTSTORE_URL'] || default
+    default_url = 'http://0.0.0.0:2113'
+    @eventstore = EventStore::Client.new ENV['EVENTSTORE_URL'] || default_url
     state = Hash.new
     EventStore::Util.poll(@eventstore, @stream).each do |event|
-      @handler.call state, event
+      next if @event_type && event[:type] != @event_type
+      @handler.call state, event, redis_client
     end
   end
   def handle stream, &blk
-    @stream = stream
+    if stream.is_a?(Hash)
+      @stream = stream.keys.first
+      @event_type = stream.values.first
+    else
+      @stream = stream
+      @event_type = nil
+    end
     @handler = blk
+  end
+  def name handler_name
+    @handler_name = handler_name
   end
   def emit stream, event_type, data
     @eventstore.write_event stream, event_type, data
   end
+  def redis_client
+    @handler_name ? namespaced_redis(@handler_name) : nil
+  end
+  def namespaced_redis namespace
+    Redis::Namespace.new(namespace, :redis => redis_connection)
+  end
+  def redis_connection
+    # will use REDIS_URL as connection string
+    @redis_connection ||= Redis.new
+  end
 end
 include StreamWorker
+
+class State
+end
+
 at_exit do
   if $!.nil?
-    puts "RUNNING"
     run!
-    puts "DONE RUNNING"
   end
 end
