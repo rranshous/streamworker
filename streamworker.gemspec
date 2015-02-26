194c9c18d265d1fcbc814720175300e2c2381720
Robby Ranshous <rranshous@gmail.com>
2015-02-25 19:11:50 -0500

    can use emit without running

--- streamworker.gemspec
+++ streamworker.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name          = 'streamworker'
-  s.version       = '0.0.5'
+  s.version       = '0.0.6'
   s.licenses      = ['BeerWare']
   s.summary       = "Work on events in a stream"
   s.description   = "Subscribe to eventstore stream, work on all events"
--- streamworker.rb
+++ streamworker.rb
@@ -3,10 +3,8 @@ require 'redis-namespace'
 
 module StreamWorker
   def run!
-    default_url = 'http://0.0.0.0:2113'
-    @eventstore = EventStore::Client.new ENV['EVENTSTORE_URL'] || default_url
     state = Hash.new
-    EventStore::Util.poll(@eventstore, @stream).each do |event|
+    EventStore::Util.poll(eventstore, @stream).each do |event|
       next if @event_type && event[:type] != @event_type
       @current_event = event
       @handler.call state, event, redis_client
@@ -22,11 +20,16 @@ module StreamWorker
     end
     @handler = blk
   end
+  def eventstore
+    return @eventstore if @eventstore
+    default_url = 'http://0.0.0.0:2113'
+    @eventstore ||= EventStore::Client.new ENV['EVENTSTORE_URL'] || default_url
+  end
   def name handler_name
     @handler_name = handler_name
   end
   def emit stream, event_type, data
-    @eventstore.write_event stream, event_type, data
+    eventstore.write_event stream, event_type, data
   end
   def redis_client
     @handler_name ? namespaced_redis(@handler_name) : nil
